121a26235b7f40e76fd184e18c9c590a979dd064
cumin <13809292481@163.com>
2026-06-16 14:34:45 +0800

    fix(zephyr): adapt xiao nrf54lm20a board to ncs 3.3.0 symbols

--- zephyr/boards/arm/xiao_nrf54lm20a/board.cmake
+++ zephyr/boards/arm/xiao_nrf54lm20a/board.cmake
@@ -1,10 +1,10 @@
 # Copyright (c) 2025 Seeed Technology Co., Ltd.
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_SOC_NRF54LM20A_ENGA_CPUAPP)
+if(CONFIG_SOC_NRF54LM20A_CPUAPP)
   board_runner_args(openocd "--cmd-load=nrf54lm20a-load" -c "targets nrf54lm20a.cpu")
   board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
-elseif(CONFIG_SOC_NRF54LM20A_ENGA_CPUFLPR)
+elseif(CONFIG_SOC_NRF54LM20A_CPUFLPR)
   board_runner_args(openocd "--cmd-load=nrf54lm20a-load" -c "targets nrf54lm20a.aux")
   board_runner_args(jlink "--speed=4000")
 endif()
